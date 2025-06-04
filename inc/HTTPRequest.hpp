14b1033aba8b0481ea447d068a623bd454d781c0
Basil Claeys <bclaeys@a1-r1-p10.s19.be>
2025-06-04 15:33:49 +0200

    Adding the HTTPRequest class.

--- inc/HTTPRequest.hpp
+++ inc/HTTPRequest.hpp
@@ -2,30 +2,38 @@
 #pragma once 
 
 #include <string>
+#include <vector>
 
 class	HTTPRequest {
 	private:
-		std::string	_method;
+		std::string _method;
 		std::string _requestTarget;
 		std::string _hostURL;
-		static const std::string HTTPVersion = "1.1";
+		std::vector <std::string> _headers;
+		std::string _body;
+
+		void		setMethod(const std::string &input);
+		void		setRequestTarget(const std::string &input);
+		void		setHostURL(const std::string &input);
+		void		setHeaders(const std::vector <std::string> &input);
+		void		setBody(const std::string &input);
 
 	public:
 		HTTPRequest();
 		HTTPRequest(std::string method,
 			std::string requestTarget,
-			std::string hostURL);
+			std::string header,
+			const std::vector <std::string> &headers,
+			std::string body);
 		HTTPRequest(const HTTPRequest &other);
 		HTTPRequest &operator=(const HTTPRequest &other);
 		~HTTPRequest();
 
-		const std::string	&getmethod() const;
-		const std::string	&getrequestTarget() const;
-		const std::string	&gethostURL() const;
-		const std::string	&getddd() const;
+		const std::string			&getMethod() const;
+		const std::string			&getRequestTarget() const;
+		const std::string			&getHostURL() const;
+		std::vector <std::string>	&getHeaders() const;
+		const std::string			&getBody() const;
 
-		void		setmethod(const std::string &input);
-		void		setrequestTarget(const std::string &input);
-		void		sethostURL(const std::string &input);
-		void		setddd(const std::string &input);
+		void parseRequest(std::string rawRequest);
 };
